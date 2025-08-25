923972fbf421353b5ffdc3254864ac04ad5ab372
ArthurOscar <arthur_o_soares@estudante.sesisenai.org.br>
2025-08-25 12:14:28 -0300

    Crud Biblioteca - BD

---
+++ banco.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE crud_biblioteca;
+
+USE crud_biblioteca;
+
+CREATE TABLE autores(
+    id_autor INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45) NOT NULL,
+    nacionalidade VARCHAR(45) NOT NULL,
+    ano_nascimento DATE NOT NULL
+);
+
+CREATE TABLE livros(
+    id_livro INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR(100) NOT NULL,
+    genero VARCHAR(100) NOT NULL,
+    ano_publicacao DATE NOT NULL,
+    fk_autor INT NOT NULL,
+    FOREIGN KEY (fk_autor) REFERENCES autores(id_autor)
+);
+
+CREATE TABLE leitores(
+    id_leitor INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45) NOT NULL,
+    email VARCHAR(200) NOT NULL,
+    telefone INT NOT NULL
+);
+
+CREATE TABLE emprestimos(
+    id_emprestimo INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    data_emprestimo DATE NOT NULL DEFAULT(CURRENT_DATE),
+    data_devolucao DATE NOT NULL,
+    fk_livro INT NOT NULL,
+    fk_leitor INT NOT NULL,
+    FOREIGN KEY (fk_livro) REFERENCES livros(id_livro),
+    FOREIGN KEY (fk_leitor) REFERENCES leitores(id_leitor)
+);
